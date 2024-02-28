1c3686b6d433e414ee9b64fd149aec6969837e13
Jubilant <Jubilantagida@gmail.com>
2024-02-29 00:42:58 +0100

    fetched data from an API to display

---
+++ app.js
@@ -0,0 +1,18 @@
+const header = document.querySelector(".header");
+const advice = document.querySelector(".advice");
+const btn = document.querySelector(".btn");
+
+const renderSlips = () => {
+
+    fetch('https://api.adviceslip.com/advice')
+        .then(resp => resp.json())
+        .then(data => {
+            const slip = data.slip;
+            header.innerText = `ADVICE ${' '} #${slip.id}`;
+            advice.innerText = `"${slip.advice}"`;
+        })
+
+}
+
+window.addEventListener('DOMContentLoaded', ()=> renderSlips());
+btn.addEventListener('click', ()=> renderSlips());
